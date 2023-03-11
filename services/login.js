a75b778ecebd02dd7615450e9d0623cceaebb89b
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2023-03-12 00:21:11 +0800

    commit form function, css format, carousel

---
+++ services/login.js
@@ -0,0 +1,48 @@
+const firstDot = document.getElementById("dotBtn1")
+const secondDot = document.getElementById("dotBtn2")
+const thirdDot = document.getElementById("dotBtn3")
+const slide1 = document.getElementById("carousel-1")
+const slide2 = document.getElementById("carousel-2")
+const slide3 = document.getElementById("carousel-3")
+const logForm = document.getElementById("login-form")
+
+    //MANUAL OF CAROUSEL
+firstDot.addEventListener("click", function(){
+    slide1.style.display = "block"
+    slide2.style.display = "none"
+    slide3.style.display = "none"
+})
+secondDot.addEventListener("click", function(){
+    slide1.style.display = "none"
+    slide2.style.display = "block"
+    slide3.style.display = "none"
+})
+thirdDot.addEventListener("click", function(){
+    slide1.style.display = "none"
+    slide2.style.display = "none"
+    slide3.style.display = "block"
+})
+
+   //AUTOMATION OF CAROUSEL
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  if (slideIndex > slides.length) {slideIndex = 1}
+  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3500);
+}
+
+logForm.addEventListener("submit",function(e){
+    e.preventDefault()
+})
