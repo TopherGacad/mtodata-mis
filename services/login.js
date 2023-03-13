8ee4aa67c1a27b6e7d374bbe3b09e29ab8751f4f
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2023-03-13 15:37:31 +0800

    update bg oppacity and class selector

--- services/login.js
+++ services/login.js
@@ -28,7 +28,7 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("img-slides");
   let dots = document.getElementsByClassName("dot");
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
