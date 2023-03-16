d16659475334e6478664ddc091ae8aa97aa23ddf
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2023-03-17 00:24:31 +0800

    trial of colors

--- services/dashboard.js
+++ services/dashboard.js
@@ -3,9 +3,20 @@ const dashBtn = document.getElementById("dash-btn")
 const memBtn = document.getElementById("member-btn")
 
 memBtn.addEventListener("click", function(){
+    //CONTENT STYLING ACTIVE
     dashContain.classList.toggle('hide')
+    
+    //BUTTON STYLING ACTIVE
+    memBtn.classList.add('btn-active')
+    dashBtn.classList.remove('btn-active')
+
 })
 dashBtn.addEventListener("click", function(){
+    //CONTENT STYLING ACTIVE
     dashContain.classList.remove('hide')
     dashContain.classList.toggle('active')
+    
+    //BUTTON STYLING ACTIVE
+    dashBtn.classList.add('btn-active')
+    memBtn.classList.remove('btn-active')
 })
