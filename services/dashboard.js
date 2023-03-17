ad1136a79707b6424c7953c1148ee8da4367494c
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2023-03-17 22:39:28 +0800

    complete button active styling

--- services/dashboard.js
+++ services/dashboard.js
@@ -1,16 +1,15 @@
 const dashContain = document.getElementById("dash-container")
 const dashBtn = document.getElementById("dash-btn")
 const memBtn = document.getElementById("member-btn")
+const userBtn = document.getElementById("users-btn")
+const financeBtn = document.getElementById("finance-btn")
+const complainBtn = document.getElementById("complain-btn")
+const programsBtn = document.getElementById("programs-btn")
+const logoutBtn = document.getElementById("logout-btn")
+const modalContain = document.getElementById("modal-container")
 
-memBtn.addEventListener("click", function(){
-    //CONTENT STYLING ACTIVE
-    dashContain.classList.toggle('hide')
-    
-    //BUTTON STYLING ACTIVE
-    memBtn.classList.add('btn-active')
-    dashBtn.classList.remove('btn-active')
 
-})
+
 dashBtn.addEventListener("click", function(){
     //CONTENT STYLING ACTIVE
     dashContain.classList.remove('hide')
@@ -18,5 +17,74 @@ dashBtn.addEventListener("click", function(){
     
     //BUTTON STYLING ACTIVE
     dashBtn.classList.add('btn-active')
+    userBtn.classList.remove('btn-active')
     memBtn.classList.remove('btn-active')
+    financeBtn.classList.remove('btn-active')
+    complainBtn.classList.remove('btn-active')
+    programsBtn.classList.remove('btn-active')
 })
+
+userBtn.addEventListener("click", function(){
+    //BUTTON STYLING ACTIVE
+    dashBtn.classList.remove('btn-active')
+    userBtn.classList.add('btn-active')
+    memBtn.classList.remove('btn-active')
+    financeBtn.classList.remove('btn-active')
+    complainBtn.classList.remove('btn-active')
+    programsBtn.classList.remove('btn-active')
+})
+
+memBtn.addEventListener("click", function(){
+    //CONTENT STYLING ACTIVE
+    dashContain.classList.toggle('hide')
+    
+    //BUTTON STYLING ACTIVE
+    dashBtn.classList.remove('btn-active')
+    userBtn.classList.remove('btn-active')
+    memBtn.classList.add('btn-active')
+    financeBtn.classList.remove('btn-active')
+    complainBtn.classList.remove('btn-active')
+    programsBtn.classList.remove('btn-active')
+})
+
+financeBtn.addEventListener("click", function(){
+    //BUTTON STYLING ACTIVE
+    dashBtn.classList.remove('btn-active')
+    userBtn.classList.remove('btn-active')
+    memBtn.classList.remove('btn-active')
+    financeBtn.classList.add('btn-active')
+    complainBtn.classList.remove('btn-active')
+    programsBtn.classList.remove('btn-active')
+})
+
+complainBtn.addEventListener("click", function(){
+    //BUTTON STYLING ACTIVE
+    dashBtn.classList.remove('btn-active')
+    userBtn.classList.remove('btn-active')
+    memBtn.classList.remove('btn-active')
+    financeBtn.classList.remove('btn-active')
+    complainBtn.classList.add('btn-active')
+    programsBtn.classList.remove('btn-active')
+})
+
+programsBtn.addEventListener("click", function(){
+    //BUTTON STYLING ACTIVE
+    dashBtn.classList.remove('btn-active')
+    userBtn.classList.remove('btn-active')
+    memBtn.classList.remove('btn-active')
+    financeBtn.classList.remove('btn-active')
+    complainBtn.classList.remove('btn-active')
+    programsBtn.classList.add('btn-active')
+})
+
+
+// logoutBtn.addEventListener("click", function(){
+//     modalContain.style.display = "block"
+    
+// })
+
+// const cancelBtn = document.getElementById("logout-cancel")
+
+// cancelBtn.addEventListener("click", function(){
+//     modalContain.style.display = "none"
+// })
