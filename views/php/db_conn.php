4f5987ddff57b510f875bb54d1e553105b672056
Jeno <Jeno>
2023-05-06 21:09:35 +0800

    duplicate echo

--- views/php/db_conn.php
+++ views/php/db_conn.php
@@ -3,13 +3,15 @@
 $sname= "localhost";
 $unmae= "root";
 $password = "";
+$port = "3307";
 
 
 $db_name = "mtodata";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $unmae, $password, $db_name, 3307);
 
 if (!$conn) {
+	echo "Connection failed!";
 	echo "Connection failed!";
 	echo "Connection failed!";
 }
