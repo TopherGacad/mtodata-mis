3e9cd30dc3b616bf46661674bcf6f306385a5e26
John Iverson Santos <johniversonsantos14@gmail.com>
2023-05-15 12:49:19 +0800

    fixing bugs

--- views/php/login.php
+++ views/php/login.php
@@ -1,14 +1,14 @@
-<?php
-session_start();
+<?php 
+session_start(); 
 include "db_conn.php";
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+	
+	die("Connection failed: " . mysqli_connect_error());
 }
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
-
+	
 	function validate($data){
        $data = trim($data);
 	   $data = stripslashes($data);
@@ -16,37 +16,38 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 	   return $data;
 	}
 
-	$email = validate($_POST['email']);
+	$uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
 
-	if (empty($email)) {
-		header("Location: ../html/login.html?error=Email is required");
+	if (empty($uname)) {
+		header("Location: ../html/login.html?error=UserName is required");
 	    exit();
 	}else if(empty($pass)){
         header("Location: ../html/login.html?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM user WHERE email='$email' AND psword='$pass'";
+		$sql = "SELECT * FROM login WHERE user_name='$uname' AND password='$pass'";
 
 		$result = mysqli_query($conn, $sql);
 
-		if (mysqli_num_rows($result) === 1) {
+		if (mysqli_num_rows($result) == 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['email'] === $email && $row['psword'] === $pass) {
-            	$_SESSION['email'] = $row['email'];
-            	$_SESSION['name'] = $row['name'];
+            if ($row['user_name'] == $uname && $row['password'] == $pass) {
+            	$_SESSION['user_name'] = $row['user_name'];
             	$_SESSION['id'] = $row['id'];
             	header("Location: ../php/dashboard.php");
+		        exit();
             }else{
-				echo '<script type="text/javascript">toastr.success("Have Fun")</script>';
-
+				header("Location: ../html/login.html?error=Incorect User name or password");
+		        exit();
 			}
 		}else{
-			echo '<script type="text/javascript">toastr.success("Have Fun")</script>';
+			header("Location: ../html/login.html?error=Incorect User name or password");
+	        exit();
 		}
 	}
 	
 }else{
-	echo '<script type="text/javascript">toastr.success("Have Fun")</script>';
-	
+	header("Location: ../html/login.html");
+	exit();
 }
