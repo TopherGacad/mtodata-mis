745572551566f442725a56757407d74994fa8a99
Christopher Gacad <124163444+TopherGacad@users.noreply.github.com>
2023-06-01 10:10:39 +0800

    update login page pathing

--- views/php/login.php
+++ views/php/login.php
@@ -15,10 +15,10 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	$pass = validate($_POST['password']);
 
 	if (empty($email)) {
-		header("Location: ../php/login.php?error=User Name is required");
+		header("Location: ../html/login.html?error=User Name is required");
 	    exit();
 	}else if(empty($pass)){
-        header("Location: ../php/login.php?error=Password is required");
+        header("Location: ../html/login.html?error=Password is required");
 	    exit();
 	}else{
 		$sql = "SELECT * FROM user WHERE email='$email' AND psword='$pass'";
@@ -33,11 +33,11 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
             	header("Location: ../php/dashboard.php");
 		        exit();
             }else{
-				header("Location: ../php/login.php?error=Incorect User name or password");
+				header("Location: ../html/login.html?error=Incorect User name or password");
 		        exit();
 			}
 		}else{
-			header("Location: ../php/login.php?error=Incorect User name or password");
+			header("Location: ../html/login.html?error=Incorect User name or password");
 	        exit();
 		}
 	}
