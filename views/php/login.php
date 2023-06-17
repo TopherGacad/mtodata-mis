402c7bdc15985602f095d8bc0ff7b55bfe43c01c
Ash <ngofarah@gmail.com>
2023-06-17 23:08:30 +0800

    account access by user role

--- views/php/login.php
+++ views/php/login.php
@@ -1,14 +1,15 @@
-<?php 
-session_start(); 
+<?php
+session_start();
 include "db_conn.php";
 
 if (isset($_POST['uname']) && isset($_POST['password'])) {
 
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
+	function validate($data)
+	{
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
 	}
 
 	$email = validate($_POST['uname']);
@@ -16,32 +17,43 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 	if (empty($email)) {
 		header("Location: ../html/login.html?error=User Name is required");
-	    exit();
-	}else if(empty($pass)){
-        header("Location: ../html/login.html?error=Password is required");
-	    exit();
-	}else{
+		exit();
+	} else if (empty($pass)) {
+		header("Location: ../html/login.html?error=Password is required");
+		exit();
+	} else {
 		$sql = "SELECT * FROM user WHERE email='$email' AND psword='$pass'";
 
 		$result = mysqli_query($conn, $sql);
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['email'] === $email && $row['psword'] === $pass) {
-            	$_SESSION['email'] = $row['email'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: ../php/dashboard.php");
-		        exit();
-            }else{
+			if ($row['email'] === $email && $row['psword'] === $pass) {
+				if ($row['roles'] === 'President') {
+					echo "Hello " . $row['roles'];
+				} else if ($row['roles'] === 'Vice President') {
+					echo "Hello " . $row['roles'];
+				} else if ($row['roles'] === 'Secretary') {
+					echo "Hello " . $row['roles'];
+				} else if ($row['roles'] === 'Treasurer' || 'Auditor') {
+					echo "Hello " . $row['roles'];
+				}
+
+				// $_SESSION['email'] = $row['email'];
+				// $_SESSION['id'] = $row['id'];
+
+				// header("Location: ../php/dashboard.php");
+				exit();
+			} else {
 				header("Location: ../html/login.html?error=Incorect User name or password");
-		        exit();
+				exit();
 			}
-		}else{
+		} else {
 			header("Location: ../html/login.html?error=Incorect User name or password");
-	        exit();
+			exit();
 		}
 	}
-	
-}else{
+
+} else {
 	header("Location: ../html/login.html");
 	exit();
 }
