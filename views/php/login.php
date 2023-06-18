42fee317212ab9ee10cc74ada5f3335f69e4efda
Ash <ngofarah@gmail.com>
2023-06-18 11:07:18 +0800

    Username as a login credential

--- views/php/login.php
+++ views/php/login.php
@@ -22,12 +22,12 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 		header("Location: ../html/login.html?error=Password is required");
 		exit();
 	} else {
-		$sql = "SELECT * FROM user WHERE email='$email' AND psword='$pass'";
+		$sql = "SELECT * FROM user WHERE email='$email' || username = '$email' AND psword='$pass'";
 
 		$result = mysqli_query($conn, $sql);
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-			if ($row['email'] === $email && $row['psword'] === $pass) {
+			if ($row['email'] || $row['username'] === $email && $row['psword'] === $pass) {
 				if ($row['roles'] === 'President') {
 					echo "Hello " . $row['roles'];
 				} else if ($row['roles'] === 'Vice President') {
