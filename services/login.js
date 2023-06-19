94997205b3b6c05956a6f72798fe51ef732ddca4
Ash <ngofarah@gmail.com>
2023-06-19 21:09:28 +0800

    Fixed error code visibility

--- services/login.js
+++ services/login.js
@@ -46,10 +46,23 @@ function showSlides() {
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
     const error = urlParams.get('error');
+    const toastContainers = document.getElementsByClassName('toast-container');
+    
     if (error === '1') {
-        const toastContainers = document.getElementsByClassName('toast-container');
         Array.from(toastContainers).forEach((container) => {
             container.style.visibility = 'visible';
+            document.getElementById("error-cont").innerText = "Error code 1: Account does not exist.";
+            
+        });
+        setTimeout(() => {
+            Array.from(toastContainers).forEach((container) => {
+                container.style.visibility = 'hidden';
+            });
+        }, 3000);
+    } else if (error === '2') {
+        Array.from(toastContainers).forEach((container) => {
+            container.style.visibility = 'visible';
+            document.getElementById("error-cont").innerText = "Error code 2: The username or password you entered is incorrect.";
         });
         setTimeout(() => {
             Array.from(toastContainers).forEach((container) => {
--- views/php/login.php
+++ views/php/login.php
@@ -15,12 +15,16 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	$email = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
 
-	$sql = "SELECT * FROM user WHERE (email='$email' OR username='$email') AND psword='$pass'";
+	$sql = "SELECT * FROM user WHERE (email='$email' OR username='$email')";
 
 	$result = mysqli_query($conn, $sql);
-	if (mysqli_num_rows($result) === 1) {
+	if (mysqli_num_rows($result) === 0) {
+		$_SESSION['login_error'] = true;
+		header("Location: ../html/login.html?error=1");
+		exit();
+	} else {
 		$row = mysqli_fetch_assoc($result);
-		if ($row['email'] || $row['username'] === $email && $row['psword'] === $pass) {
+		if ($row['psword'] === $pass) {
 			if ($row['roles'] === 'President') {
 				echo "Hello " . $row['roles'];
 			} else if ($row['roles'] === 'Vice President') {
@@ -33,17 +37,13 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 			// $_SESSION['email'] = $row['email'];
 			// $_SESSION['id'] = $row['id'];
-
 			// header("Location: ../php/dashboard.php");
+			exit();
+		} else {
+			$_SESSION['login_error'] = true;
+			header("Location: ../html/login.html?error=2");
 			exit();
 		}
 
-	} else if (mysqli_num_rows($result) === 0) {
-		$_SESSION['login_error'] = true;
-		header("Location: ../html/login.html?error=1");
-		exit();
-	} else {
-		header("Location: ../html/login.html");
-		exit();
 	}
 }
