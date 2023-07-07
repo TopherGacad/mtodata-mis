0dae27ad24e6950264a3a1b58f5ae11cb83cc867
Ash <ngofarah@gmail.com>
2023-07-07 13:10:38 +0800

    Fixed the toast-not-showing bug

--- services/unitinfo.js
+++ services/unitinfo.js
@@ -1,3 +1,61 @@
+
+// Wrap the code inside a function to ensure it executes after the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const suc = urlParams.get('success');
+    const toastContainers = document.getElementById('toast-success');
+    const toastMessage = document.getElementById('success-con');
+
+    if (suc === '0') {
+        toastContainers.style.visibility = 'visible';
+        document.getElementById('success-con').innerHTML = "<strong>Successful</strong> Unit information added.";
+        setTimeout(() => {
+            toastContainers.style.visibility = 'hidden';
+        }, 3000);
+    }
+
+    if (suc === 'true') {
+        
+        toastContainers.style.visibility = 'visible';
+        toastMessage.innerHTML = `<strong>Success</strong> donor information added.`;
+
+        setTimeout(() => {
+            toastContainers.style.visibility = 'hidden';
+        }, 3000);
+    }
+
+    if (suc === 'true%unit') {
+
+        toastContainers.style.visibility = 'visible';
+        toastMessage.innerHTML = `<strong>Success</strong> check the updated unit info.`;
+
+        console.log('hehe');
+
+        setTimeout(() => {
+            toastContainers.style.visibility = 'hidden';
+        }, 3000);
+    }
+
+});
+
+window.onload = function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const memberID = urlParams.get('id');
+
+    if (memberID) {
+        urlParams.delete("success");
+        const newUrl = window.location.pathname + "?" + urlParams.toString();
+        history.replaceState({}, document.title, newUrl);
+
+        console.log('$memberID');
+    }
+}; 
+
+
+
+
+
+
 //UNIT INFO SEARCH
 const unitSearch = document.getElementById('unit-search');
 const unitTableBody = document.getElementById('unit-table-body');
@@ -60,64 +118,3 @@ donorSearch.addEventListener('input', () => {
         }
     }
 });
-
-// Wrap the code inside a function to ensure it executes after the DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const suc = urlParams.get('success');
-    const toastContainers = document.getElementById('toast-success');
-
-    if (suc === '0') {
-        toastContainers.style.visibility = 'visible';
-        document.getElementById('success-con').innerHTML = `<strong>Successful</strong> Unit information added.`;
-        setTimeout(() => {
-            toastContainers.style.visibility = 'hidden';
-        }, 3000);
-    }
-
-    if (success === 'true') {
-        const toastContainer = document.getElementById('toast-success');
-        const toastMessage = document.getElementById('success-con');
-
-        toastContainer.style.visibility = 'visible';
-        toastMessage.innerHTML = `<strong>Success</strong> donor information added.`;
-
-        setTimeout(() => {
-            toastContainer.style.visibility = 'hidden';
-        }, 3000);
-    }
-
-    if (success === 'true%unit') {
-        const toastContainer = document.getElementById('toast-success');
-        const toastMessage = document.getElementById('success-con');
-
-        toastContainer.style.visibility = 'visible';
-        toastMessage.innerText = `<strong>Success</strong> check the updated unit info.`;
-
-        console.log('hehe');
-
-        setTimeout(() => {
-            toastContainer.style.visibility = 'hidden';
-        }, 3000);
-    }
-
-});
-
-window.onload = function () {
-    if (window.location.search.includes("success")) {
-        history.replaceState({}, document.title, window.location.pathname);
-    }
-}
-
-window.onload = function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const memberID = urlParams.get('id');
-
-    if (memberID) {
-        urlParams.delete("success");
-        const newUrl = window.location.pathname + "?" + urlParams.toString();
-        history.replaceState({}, document.title, newUrl);
-
-        console.log('$memberID');
-    }
-}; 
