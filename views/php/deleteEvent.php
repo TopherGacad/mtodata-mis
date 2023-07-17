e423e02db540a3d66f69acd6b23bdec058b513ab
EntityUnknown <johniversonsantos14@gmail.com>
2023-07-17 08:38:54 +0800

    Delete Event Function in dashboard Added Delete Event file for row deletion in database

---
+++ views/php/deleteEvent.php
@@ -0,0 +1,26 @@
+<?php
+// connect to the MySQL database
+include "db_conn.php";
+
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// retrieve the member ID from the AJAX request
+$id = $_POST["id"];
+
+// Prepare and execute the SQL queries to delete the rows from the respective tables
+$sql = "DELETE FROM events_programs WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+    // If deletion was successful, return success message
+    echo "Event deleted successfully.";
+} else {
+    // If deletion failed, return error message
+    echo "Error deleting Complaint: " . $conn->error;
+}
+
+// close MySQL connection
+$conn->close();
+?>
